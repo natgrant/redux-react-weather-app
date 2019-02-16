2b8a8c93193698d4abf966743a2eeecf5c1a099f
Nat Grant <email.natgrant@gmail.com>
2019-02-17 11:16:01 +1300

    now showing error if no city entered

--- src/container/SearchBar.jsx
+++ src/container/SearchBar.jsx
@@ -8,7 +8,8 @@ class SearchBar extends Component {
     super(props);
 
     this.state = {
-      term: ""
+      term: "",
+      error: false
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
@@ -18,30 +19,42 @@ class SearchBar extends Component {
     this.setState({ term: e.target.value });
   }
 
-  onFormSubmit(e) {
+  onFormSubmit = e => {
     e.preventDefault();
 
-    // We need to go and fetch weather data here
-    this.props.fetchWeather(this.state.term);
-    this.setState({ term: "" });
-  }
+    // We need to go and fetch weather data here:
+    if (this.state.term === "") {
+      this.setState({
+        error: true
+      });
+    } else {
+      this.props.fetchWeather(this.state.term);
+      this.setState({ term: "" });
+    }
+  };
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
-        <input
-          placeholder="Get a 5 day forecast in your favourite cities"
-          className="form-control"
-          type="text"
-          value={this.state.term}
-          onChange={this.handleInputChange}
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-info">
-            Submit!
-          </button>
-        </span>
-      </form>
+      <div>
+        {" "}
+        <form onSubmit={this.onFormSubmit} className="input-group">
+          <input
+            placeholder="Get a 5 day forecast in your favourite cities"
+            className="form-control"
+            type="text"
+            value={this.state.term}
+            onChange={this.handleInputChange}
+          />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-info">
+              Submit!
+            </button>
+          </span>
+        </form>
+        {this.state.error && (
+          <p className="error-null-entry">Please enter a city...</p>
+        )}
+      </div>
     );
   }
 }
--- src/container/WeatherList.jsx
+++ src/container/WeatherList.jsx
@@ -31,17 +31,19 @@ class WeatherList extends Component {
 
   render() {
     return (
-      <table className="table table-hover table-striped">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (K)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
-      </table>
+      <div>
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature (K)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
+            </tr>
+          </thead>
+          <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+        </table>
+      </div>
     );
   }
 }
